0eea0950e3b988a5bdf648406231a51ca900c4a6
Ivan Matveev <imatveev@delimobil.ru>
2022-09-29 17:34:32 +0300

    extract data from my_sql

--- scripts/Python/sql_queries.py
+++ scripts/Python/sql_queries.py
@@ -1,6 +1,25 @@
 from clickhouse_driver import Client
-from db_credentials import clickhouse_db_config
+import mysql.connector
+from db_credentials import my_sql_db_config, clickhouse_db_config
 
-client = Client(**clickhouse_db_config)
+clickhouse_client = Client(**clickhouse_db_config)
+con = mysql.connector.connect(**my_sql_db_config)
 
-print(client.execute('SHOW DATABASES'))
+
+cursor = con.cursor()
+
+my_sql_extract = """
+SELECT
+    Rerurned
+    ,Order_id
+FROM
+    returns
+"""
+
+cursor.execute(my_sql_extract)
+
+clickhouse_client.execute("INSERT INTO SAE.returns (Rerurned,Order_id) VALUES",
+                         ((Rerurned,Order_id ) for Rerurned,Order_id in cursor))
+
+cursor.close()
+con.close()
