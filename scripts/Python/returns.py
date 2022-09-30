552899607d27d4e7afcfade14caad93c41ebffe1
Ivan Matveev <imatveev@delimobil.ru>
2022-09-30 17:37:57 +0300

    implement returns.py

--- scripts/Python/returns.py
+++ scripts/Python/returns.py
@@ -1,24 +1,6 @@
-from clickhouse_driver import Client
-import mysql.connector
-from db_credentials import my_sql_db_config, clickhouse_db_config
+from etl import ETLBase
+from querise import my_sql_extract
 
-clickhouse_client = Client(**clickhouse_db_config)
-con = mysql.connector.connect(**my_sql_db_config)
+orders = ETLBase('mysql','returns','SAE')
 
-cursor = con.cursor()
-
-my_sql_extract = """
-SELECT
-    *
-FROM
-    returns
-"""
-
-cur = cursor.execute(my_sql_extract)
-
-clickhouse_client.execute("INSERT INTO SAE.returns  VALUES",
-                            ((cur)for cur in cursor)
-                                )
-
-cursor.close()
-con.close()
+orders.do_insert(my_sql_extract)
