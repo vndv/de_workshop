f4e33229a999da32c52360d0ffb9d816d6e088f5
Ivan Matveev <imatveev@delimobil.ru>
2022-09-30 17:36:28 +0300

    implement orders.py

--- scripts/Python/orders.py
+++ scripts/Python/orders.py
@@ -1,26 +1,6 @@
-from psycopg2 import connect
-from clickhouse_driver import Client
-from db_credentials import pg_db_config, clickhouse_db_config
+from etl import ETLBase
+from querise import pg_extract
 
-settings = {'input_format_null_as_default': True, 'types_check': True}
+orders = ETLBase('postgres','orders','orders','SAE')
 
-pg_connection = connect(**pg_db_config)
-clickhouse_client = Client(**clickhouse_db_config,settings=settings)
-
-cur = pg_connection.cursor()
-
-pg_extract = """select
-  *
-from
-    public.orders;
-"""
-
-cur.execute(pg_extract)
-result = cur.fetchall()
-
-clickhouse_client.execute("INSERT INTO SAE.orders  VALUES",
-                            ((res)for res in result)
-                                )
-
-cur.close()
-pg_connection.close()
+orders.do_insert(pg_extract)
