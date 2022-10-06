45d43db2acafa0f506c0ce4d217c0e1d6abd1e2c
Ivan Matveev <imatveev@delimobil.ru>
2022-10-06 22:31:18 +0300

    reformat project structure

--- scripts/Python/orders.py
+++
@@ -1,6 +0,0 @@
-from etl import ETLBase
-from querise import pg_extract
-
-orders = ETLBase('postgres','orders','SAE')
-
-orders.do_insert(pg_extract)
--- scripts/Python/returns.py
+++
@@ -1,6 +0,0 @@
-from etl import ETLBase
-from querise import my_sql_extract
-
-returns = ETLBase('mysql','returns','SAE')
-
-returns.do_insert(my_sql_extract)
