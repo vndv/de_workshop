5e012ee1e8df7f7736e570b78958c93a6baeb1cb
Ivan Matveev <imatveev@delimobil.ru>
2022-10-06 22:27:58 +0300

    implement airflow

---
+++ Airflow/dags/extract_data.py
@@ -0,0 +1,37 @@
+from pathlib import PurePath
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+
+dag_name = PurePath(__file__).name.split('.')[0]
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 9, 1),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+    'max_active_runs': 1,
+    'email': ['test@mail.ru']
+}
+
+dag = DAG(
+    dag_name,
+    default_args=default_args,
+    schedule_interval='* * * * *',
+    catchup=False,
+)
+
+orders = BashOperator(
+    task_id='orders',
+    bash_command='python /opt/airflow/Python/orders.py',
+    dag=dag,
+)
+
+returns = BashOperator(
+    task_id='returns',
+    bash_command='python /opt/airflow/Python/returns.py',
+    dag=dag,
+)
---
+++ Airflow/scripts/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+airflow db upgrade
+
+airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
+
+airflow webserver
